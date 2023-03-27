640945b5d27dfb64424dbe87336cb3f29b92c322
Sumanth Bolisetty <bsumanth02@gmail.com>
2023-03-27 12:13:40 +0530

    website-commit

--- main.py
+++ main.py
@@ -42,4 +42,4 @@ def api_currenttrends():
 @app.route('/home', methods=['GET'])
 def api_homepage():
     return render_template("website.html")
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+app.run(host="0.0.0.0", port=8081)
